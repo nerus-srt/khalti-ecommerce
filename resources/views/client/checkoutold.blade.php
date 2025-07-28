28b29be2a816982276cbf091c515840fd0bc3a80
nerus-srt <nerus.srt@outlook.com>
2025-07-28 05:54:26 -0400

    first change to old blade checkout

--- resources/views/client/checkoutold.blade.php
+++ resources/views/client/checkoutold.blade.php
@@ -1,7 +1,7 @@
 <x-template.layout title="{{ $title }}" >
   <x-organisms.navbar :path="$shop->path"/>
   <div class="container mt-3">
-    <h2>Checkout</h2>
+    <h2>Checkout here</h2>
     <hr/>
   </div>
   <x-organisms.carts />
